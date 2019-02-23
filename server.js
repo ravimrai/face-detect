73dbd8025d59aed748174cad082a9c708736c7e4
ravimrai <ravimadhav1707@gmail.com.com>
2019-02-23 23:21:05 +0530

    changing db

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ const db = knex({
 
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.send(database.users));
+app.get('/', (req, res) => res.send('It is working'));
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${process.env.PORT}`))
