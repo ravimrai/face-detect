24d5bcb452cce91c8ee4e03378f044d2e7f6fed9
ravimrai <ravimadhav1707@gmail.com.com>
2019-02-23 22:02:55 +0530

    adding

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+var cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'SHINIGAMI',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send(database.users));
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
+app.put('/image', (req, res) => { image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
