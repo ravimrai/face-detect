90fcd700142ece4d34b4464f9f02600fda0c0bca
ravimrai <ravimadhav1707@gmail.com.com>
2019-02-24 10:37:23 +0530

    adding db env

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-shaped-29392',
-    user : 'SHINIGAMI',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
